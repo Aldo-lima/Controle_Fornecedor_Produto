4622aa6e090d140152ec6e7e29babf1d752d7551
Aldo Cesar <57098697+Aldo-lima@users.noreply.github.com>
2021-09-10 20:00:54 -0300

    models

---
+++ Controle_Fornecedor_Produto/fornecedor/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Fornecedor, Contato, Categoria
+
+
+admin.site.register(Fornecedor)
+admin.site.register(Contato)
+admin.site.register(Categoria)
+
+
